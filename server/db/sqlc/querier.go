1161c14b63238a23a9487c50f1e321c14b76e332
Neezar <rforlyffe@gmaicl.com>
2022-04-06 14:54:45 +0800

    add account testing and transaction test

--- server/db/sqlc/querier.go
+++ server/db/sqlc/querier.go
@@ -11,7 +11,7 @@ type Querier interface {
 	CreateEntry(ctx context.Context, arg CreateEntryParams) (Entry, error)
 	CreateTransfer(ctx context.Context, arg CreateTransferParams) (Transfer, error)
 	DeleteAccount(ctx context.Context, id int64) error
-	GetAccounts(ctx context.Context, id int64) (Account, error)
+	GetAccount(ctx context.Context, id int64) (Account, error)
 	GetEntry(ctx context.Context, id int64) (Entry, error)
 	GetTransfer(ctx context.Context, id int64) (Transfer, error)
 	ListAccounts(ctx context.Context, arg ListAccountsParams) ([]Account, error)
