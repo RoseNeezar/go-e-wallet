4da3f288296a323502a5000c14cd13d5dede7009
Neezar <rforlyffe@gmaicl.com>
2022-04-08 14:36:25 +0800

    add more transaction test and github actions

--- server/db/query/account.sql
+++ server/db/query/account.sql
@@ -5,12 +5,16 @@ INSERT INTO accounts (
   currency
 ) VALUES (
   $1, $2, $3
-)
-RETURNING *;
+) RETURNING *;
 
 -- name: GetAccount :one
 SELECT * FROM accounts
 WHERE id = $1 LIMIT 1;
+
+-- name: GetAccountForUpdate :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1
+FOR NO KEY UPDATE;
 
 -- name: ListAccounts :many
 SELECT * FROM accounts
@@ -20,10 +24,17 @@ LIMIT $2
 OFFSET $3;
 
 -- name: UpdateAccount :one
-UPDATE accounts 
+UPDATE accounts
 SET balance = $2
 WHERE id = $1
 RETURNING *;
 
+-- name: AddAccountBalance :one
+UPDATE accounts
+SET balance = balance + sqlc.arg(amount)
+WHERE id = sqlc.arg(id)
+RETURNING *;
+
 -- name: DeleteAccount :exec
-DELETE FROM accounts WHERE id = $1;
+DELETE FROM accounts
+WHERE id = $1;
